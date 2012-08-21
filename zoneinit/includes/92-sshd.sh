650fde7f005a19214d41aeee4ce02bcd14310058
Filip Hajny <filip@hajny.net>
2012-08-21 11:27:56 +0200

    Change the way how SSH host keys are handled. It is preferred to store keys under /var/ssh to let the SMF method for network/ssh automatically re-create them if missing.

--- zoneinit/includes/92-sshd.sh
+++ zoneinit/includes/92-sshd.sh
@@ -1,8 +1,17 @@
-log "generating a new pair of SSH keys"
+# Old style SSH host keys were stored under /etc/ssh and needed to be
+# recreated using /lib/svc/method/sshd -c.
+#
+# New (SmartOS) style SSH host keys are stored under /var/ssh and are
+# automatically recreated by the SMF method if missing.
+#
+# We assume here that the old keys were already deleted previously
+# (e.g. by sm-prepare-image).
 
-rm -f /etc/ssh/ssh_host_*
-/lib/svc/method/sshd -c >/dev/null || \
-  error "SSH key refresh failed."
+if awk '/^HostKey/ {print $2}' /etc/ssh/sshd_config | grep '/etc/ssh' > /dev/null; then
+  log "generating a new pair of SSH keys"
+  /lib/svc/method/sshd -c >/dev/null || \
+    error "SSH key refresh failed."
+fi
 
 if [ ${SSH_ALLOW_PASSWORDS} ]; then
   log "enabling password authentication in SSH"
