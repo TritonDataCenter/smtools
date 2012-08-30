103631ce419c52ae45267ca68459848ca3667292
Filip Hajny <filip@hajny.net>
2012-08-30 22:11:21 +0200

    Change the way how passwords are retrieved from metadata. Populate ADMIN_PW kind of variables from metadata in 04-metadata.sh, and set system passwords from these variables in 91-passwords.sh.

--- zoneinit/includes/91-passwords.sh
+++ zoneinit/includes/91-passwords.sh
@@ -1,16 +1,9 @@
-: ${USERS=admin root}
-USERS=(${USERS})
-
 for USER in ${USERS[@]}; do
 
   log "setting system password for user '${USER}'"
   PASS_VAR_LOWER=${USER}_pw
   PASS_VAR_UPPER=$(echo ${PASS_VAR_LOWER} | tr '[a-z]' '[A-Z]')
-
   USER_PW=${!PASS_VAR_UPPER}
-  if [ ! ${USER_PW} ] && [ ${HAS_METADATA} ]; then
-    USER_PW=$(mdata-get ${PASS_VAR_LOWER} 2>/dev/null) || unset USER_PW
-  fi
 
   if [ "${USER_PW}" ] && \
      [ -e "$(type -p genbfpw)" ] && [ -e "$(type -p changepass)" ]; then
