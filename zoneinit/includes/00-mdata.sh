799cef33ecb7bfbe13dfcb2dfc0f120d9bbc6c5b
Filip Hajny <filip@hajny.net>
2013-06-21 13:35:30 +0200

    Add support for the new metadata socket location (DATASET-795). Better deal with the new metadata exit codes.

--- zoneinit/includes/00-mdata.sh
+++ zoneinit/includes/00-mdata.sh
@@ -3,9 +3,13 @@ if [ -x /usr/sbin/mdata-get ]; then
 
   log "waiting for metadata to show up"
 
-  until [ -e /var/run/smartdc/metadata.sock ] || [ $((MCOUNT++)) -gt 30 ]; do
+  until [ -e /.zonecontrol/metadata.sock ] ||\
+        [ -e /var/run/smartdc/metadata.sock ] ||\
+        [ $((MCOUNT++)) -gt 30 ]; do
     sleep 1
   done
 
-  [ -e /var/run/smartdc/metadata.sock ] || log "metadata failed to show up"
+  [ -e /.zonecontrol/metadata.sock ] ||\
+  [ -e /var/run/smartdc/metadata.sock ] ||\
+    log "metadata failed to show up"
 fi
